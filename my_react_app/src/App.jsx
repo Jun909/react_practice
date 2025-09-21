efad86465bb8de5a8c9e5ebd038909de7a59768e
Jun909 <junsiang97@hotmail.com>
2025-09-21 16:36:19 +0200

    Complete a simple chatbot style app

--- my_react_app/src/App.jsx
+++ my_react_app/src/App.jsx
@@ -4,21 +4,50 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   function ChatInput() {
     return (
-      <div>
-        <input />
+      <>
+        <input placeholder='Enter your message here' size={30}/>
         <button>Send</button>
-      </div>      
+      </>
+            
     );
   }
 
+  function ChatMessage(props) {
+    // const message = props.message;
+    // const sender = props.sender;
+    const { message, sender } = props;
+
+    if (sender === "robot") {
+      return (
+        <div>
+          <img src="robot.png" width={50}/>
+          {message}          
+        </div>
+      )
+    }
+    return (
+      <div>
+        {message}
+        <img src="user.png" width={50}/>
+      </div>
+    )
+  }
+
   return (
-    <div>
+    <>
       {ChatInput()}
-    </div>
+      <ChatMessage message="Hello Chatbot" sender="user"/>
+      <ChatMessage message="Hello, how can I help you?" sender="robot"/>
+      <ChatMessage message="Can you get me today's date?" sender="user"/>
+      <ChatMessage message="Today is 27th of September" sender="robot"/>
+      <ChatMessage message="How about flip a coin?" sender="user"/>
+      <ChatMessage message="Sure, you got tails :)" sender="robot"/>
+    </>
+    
   )
 }
 
