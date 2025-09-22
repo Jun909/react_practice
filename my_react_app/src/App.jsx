20e24f06403482c1d4530343ef9cb32395a0aea7
Jun909 <junsiang97@hotmail.com>
2025-09-22 21:11:31 +0200

    Learn and introduce state

--- my_react_app/src/App.jsx
+++ my_react_app/src/App.jsx
@@ -4,48 +4,111 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
+    const [chatMessages, setChatMessages] = useState([{
+      message: "Hello Chatbot", sender: "user", id:"id1"
+    }, {
+      message: "Hello, how can I help you?", sender: "robot", id:"id2"
+    },
+    {
+      message: "Can you get me today's date?", sender: "user", id:"id3"
+    },
+    {
+      message: "Today is 27th of September", sender: "robot", id:"id4"
+    },
+    {
+      message: "How about flip a coin?", sender: "user", id:"id5"
+    },
+    {
+      message: "Sure, you got tails :)", sender: "robot", id:"id6"
+    }
+    ]);
 
-  function ChatInput() {
+  function ChatMessages() {
+    // const [chatMessages, setChatMessages] = useState([{
+    //   message: "Hello Chatbot", sender: "user", id:"id1"
+    // }, {
+    //   message: "Hello, how can I help you?", sender: "robot", id:"id2"
+    // },
+    // {
+    //   message: "Can you get me today's date?", sender: "user", id:"id3"
+    // },
+    // {
+    //   message: "Today is 27th of September", sender: "robot", id:"id4"
+    // },
+    // {
+    //   message: "How about flip a coin?", sender: "user", id:"id5"
+    // },
+    // {
+    //   message: "Sure, you got tails :)", sender: "robot", id:"id6"
+    // }
+    // ]);
+
     return (
       <>
-        <input placeholder='Enter your message here' size={30}/>
-        <button>Send</button>
+        {chatMessages.map((chatMessage) => {
+          return (
+          <ChatMessage 
+          message={chatMessage.message} 
+          sender={chatMessage.sender} 
+          key={chatMessage.id}/>
+          );
+        })}
+      </>
+    )
+    };
+
+  function ChatInput() {
+
+    const [inputText, setInputText] = useState("");
+
+    function saveInputText(event) {
+      setInputText(event.target.value);
+    }
+
+    function sendMessage(){
+      setChatMessages([...chatMessages, {message: inputText, sender: "user", id: crypto.randomUUID()}]);
+
+      return (
+        <>
+          {chatMessages.map((chatMessage) => {
+            return (
+            <ChatMessage 
+            message={chatMessage.message} 
+            sender={chatMessage.sender} 
+            key={chatMessage.id}/>
+            );
+          })}
+        </>
+        
+      )
+    }
+
+    return (
+      <>
+        <input placeholder='Enter your message here' 
+        size={30} 
+        onChange={saveInputText}/>
+        <button onClick={sendMessage}>Send</button>
       </>
             
     );
   }
 
-  function ChatMessage(props) {
-    // const message = props.message;
-    // const sender = props.sender;
-    const { message, sender } = props;
+  function ChatMessage({message, sender}) {
 
-    if (sender === "robot") {
-      return (
-        <div>
-          <img src="robot.png" width={50}/>
-          {message}          
-        </div>
-      )
-    }
     return (
       <div>
+        { sender === "robot" && <img src="robot.png" width={50}/> }
         {message}
-        <img src="user.png" width={50}/>
+        { sender === "user" && <img src="user.png" width={50}/> }
       </div>
     )
   }
 
   return (
     <>
-      {ChatInput()}
-      <ChatMessage message="Hello Chatbot" sender="user"/>
-      <ChatMessage message="Hello, how can I help you?" sender="robot"/>
-      <ChatMessage message="Can you get me today's date?" sender="user"/>
-      <ChatMessage message="Today is 27th of September" sender="robot"/>
-      <ChatMessage message="How about flip a coin?" sender="user"/>
-      <ChatMessage message="Sure, you got tails :)" sender="robot"/>
+      <ChatInput />
+      {/* <ChatMessages/> */}
     </>
     
   )
